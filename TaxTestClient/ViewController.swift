bb884e865120621c9e89c1415f2c64fafe261774
Willy Breitenbach <willybr@me.com>
2020-10-15 13:54:11 +0200

    Initial Commit

---
+++ TaxTestClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxTestClient
+//
+//  Created by Willy Breitenbach on 15.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
